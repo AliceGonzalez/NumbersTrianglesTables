132a9fef01780d19ed5262d9565aa3942f3fc113
AliceGonzalez <alicegcodes@gmail.com>
2024-03-05 22:05:44 -0500

    Started Triangle

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,11 +3,24 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 1; i < numberOfRows; i++) {
+            for (int j = 0; j < i; j++) {
+                result.append("*");
+            }
+            result.append("\n");
+        }
+        return result.toString();
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < numberOfStars; i++){
+            result.append("*");
+        }
+        return result.toString();
     }
 
     public static String getSmallTriangle() {
