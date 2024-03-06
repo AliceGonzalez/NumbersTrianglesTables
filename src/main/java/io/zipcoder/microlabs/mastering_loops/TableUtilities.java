683b17d33bf317376d8470656e4e46f3836c74e2
AliceGonzalez <alicegcodes@gmail.com>
2024-03-05 21:16:38 -0500

    Table Test completed

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,40 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 1; i <= 5; i++){
+            for(int j = 1; j <= 5; j++){
+                String cell = String.format("%3d |", i * j);
+                result.append(cell);
+            }
+                   result.append("\n");
+        }
+        return result.toString();
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i <= 10; i++){
+            for(int j = 1; j <= 10; j++){
+                String cell = String.format("%3d |", i * j);
+                result.append(cell);
+            }
+            result.append("\n");
+        }
+        return result.toString();
+
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i <= 20; i++){
+            for(int j = 1; j <= 20; j++){
+                String cell = String.format("%3d |", i * j);
+                result.append(cell);
+            }
+            result.append("\n");
+        }
+        return result.toString();
     }
 }
